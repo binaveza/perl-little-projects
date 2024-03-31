0e9af61aff3e70c22bb7f806ff5f29607e283102
Olya <107501158+binaveza@users.noreply.github.com>
2024-03-31 14:12:01 +0300

    add leap year checker

---
+++ Leap Year Check/leap_year_check.pl
@@ -0,0 +1,36 @@
+use strict;
+use warnings;
+
+print "Hello! Let's check for leap year.\n";
+
+sub checking {
+
+    #print "Hello! Let's check for leap year.\n";
+    print "Enter year (1600-4000): ";
+
+    my $year = <STDIN>;
+    chomp($year);
+
+    if ($year >= 1600 && $year <= 4000) {
+        if (($year % 4 == 0 && $year % 100 != 0) || $year % 400 == 0) {
+            print "Result: $year - leap year\n";
+        } else {
+            print "Result: $year - not a leap year\n";
+        }
+    } else {
+        print "Year is not included in the range from 1600 to 4000\n";
+    }
+
+    print "Do you want to check other year? [y/n]";
+    my $new_try = <STDIN>;
+    chomp($new_try);
+    
+    if ($new_try eq 'y') {
+        checking();
+    } elsif($new_try ne 'n') {
+        print "Uncorrect enter. The programm is ending.";
+    }
+
+}
+
+checking();
